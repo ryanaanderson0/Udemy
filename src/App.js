29b6247166d7b64865fcbf8d1e67e03d23bf5d0e
Ryan Anderson <ryanaanderson0@gmail.com>
2020-08-19 09:48:22 -0700

    Lecture 47: two way binding. Added another event handler on the text input box. It doesn't show the changes yet, I need to double check the bindings. The first name value should reflect the changes made to the text input box

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        persons: [
+            {name: 'Ryan', age: 29},
+            {name: 'Camern', age: 29},
+            {name: 'Viola', age: 2},
+            {name: 'Jimmy', age: '?'}
+        ],
+        otherPersons: 'some other value' //changing the state will not change or discard other objects in the state
+    }
+
+    switchNameHandler = () => {
+        //console.log('Was clicked!');
+        //DONT DO THIS this.state.persons[0].name = 'Bryan';
+        this.setState( {
+            persons: [
+                {name: 'Bryan', age: 29},
+                {name: 'Cameron', age: 29} 
+            ] 
+        } )
+    }
+
+    nameChangeHandler = (event) => {
+        this.setState( {
+            persons: [
+                {name: event.target.value, age: 29},
+                {name: 'Cameron', age: 29} 
+            ] 
+        } )
+    }
+    
+    // only works in classes
+    render() {
+        return(
+        <div className="App">
+            <h1>Hi, I'm a React App</h1>
+            <button onClick={this.switchNameHandler}> Switch Name</button>
+            <Person 
+                name={this.state.persons[0].name} 
+                age={this.state.persons[0].age}
+                click={this.switchNameHandler}>  My hobbies include: Playing Video Games, Cuddling, and Cooking </Person> 
+            <Person 
+                name={this.state.persons[1].name} 
+                age={this.state.persons[1].age} >  My hobbies include: Coding, Playing Guitar, and Sleeping </Person>  
+            <Person name="Viola" age="2" >  My hobbies include: Hoofing, Cuddling, and Playing </Person>
+            <Person name="Jimmy" age="?" > My hobbies include: Snarling, Cuddling, and Sleeping</Person> 
+        </div>
+    );
+    }
 }
 
 export default App;
