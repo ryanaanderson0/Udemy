5111b069a1c80c2508b0e753982a892a25dd4b88
Ryan Anderson <ryanaanderson0@gmail.com>
2020-09-01 11:49:45 -0700

    Lesson 85:A Better Project Structure-- For a better project folder structure, I created a components folder for the Person.js and persons.js. I created a containers folder for the App.js, App.css, and App.test.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
 
 const StyledPrimaryButton = styled.button`
     background-color: blue;
